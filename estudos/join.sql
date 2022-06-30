86a99eb313683a28132741dcd46b63bf0a3591e5
GabrielDvPereira <gdavi.gd@gmail.com>
2022-06-30 08:38:47 -0300

    add study for join

---
+++ estudos/join.sql
@@ -0,0 +1,12 @@
+-- INNER JOIN
+---  Junçao de duas tabelas que tem pontos em comum
+---  Todos os registros preenchidos dos dois lados
+
+-- LEFT JOIN (ou LEFT OUTER)
+--- Junçao de duas tablelas, mas traz dados somente da tabela da esquerda
+
+-- RIGHT JOIN (ou RIGHT OUTER)
+--- Juncao de duas tabelas, mas tras os dados da tabela a direita
+
+-- FULL JOIN
+--- Uniao entre right e left
